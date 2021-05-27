da6403f4da81dd204395118233ab16796f970e20
Sergey Sovgut <sergey.sovgut@outlook.com>
2021-05-27 14:50:06 +0300

    Fixed issue with contexts

--- src/Components/Drawer/index.js
+++ src/Components/Drawer/index.js
@@ -1,96 +1,111 @@
-import { useState, useContext } from 'react';
-import { Drawer, Button, Input, Table } from 'antd';
-import { EllipsisOutlined, CloseOutlined } from '@ant-design/icons';
-import { FormContext } from '../../Context/form.context';
-import { DrawerContext } from '../../Context/drawer.context';
-import { departmentColumns, categoryColumns} from '../utils/columns';
-import { departmentData, categoryData} from '../utils/data';
+import { useState, useContext } from "react";
+import { Drawer, Button, Input, Table } from "antd";
+import { EllipsisOutlined, CloseOutlined } from "@ant-design/icons";
+import { FormContext } from "../../Context/form.context";
+import { DrawerContext } from "../../Context/drawer.context";
+import { departmentColumns, categoryColumns } from "../utils/columns";
+import { departmentData, categoryData } from "../utils/data";
 
+export default function DrawerComponent({
+  name,
+  value = {},
+  onChange /* type */,
+}) {
+  console.log("value: ", value);
+  const [visible, setVisible] = useState(false);
+  const [content /* setContent */] = useState(null);
+  const formContext = useContext(FormContext);
+  const drawerContext = useContext(DrawerContext);
 
-export default function DrawerComponent({ value = {}, onChange, /* type */ }) {
-    console.log('value: ', value);
-    const [visible, setVisible] = useState(false);
-    const [content, /* setContent */] = useState(null);
-    const formContext = useContext(FormContext);
-    const drawerContext = useContext(DrawerContext);
+  const showDrawer = () => {
+    setVisible(true);
+  };
+  const onClose = () => {
+    setVisible(false);
+  };
 
-    const showDrawer = () => {
-        setVisible(true);
-    };
-    const onClose = () => {
-        setVisible(false);
-    };
+  let columns,
+    data = null;
+  columns =
+    drawerContext.type === "department"
+      ? departmentColumns()
+      : categoryColumns();
+  data =
+    drawerContext.type === "department" ? departmentData() : categoryData();
 
-    let columns, data = null; 
-    columns = drawerContext.type === 'department' ? departmentColumns() : categoryColumns();
-    data = drawerContext.type === 'department' ? departmentData() : categoryData();
+  const onClickRow = (record) => {
+    console.log("record: ", record);
+    onContentChange(record[drawerContext.type]);
+  };
 
-    const onClickRow = (record) => {
-        console.log('record: ', record);
-        onContentChange(record.department);
-    };
+  const triggerChange = (changedValue) => {
+    onChange?.({
+      content,
+      ...value,
+      ...changedValue,
+    });
+  };
 
-    const triggerChange = (changedValue) => {
-        onChange?.({
-            content,
-            ...value,
-            ...changedValue,
-        });
-    };
+  const onContentChange = (value) => {
+    const newContent = value;
 
-    const onContentChange = (value) => {
-        const newContent = value;
-
-        if (value !== null) {
-            formContext.сhangeContent(newContent);
-            // setContent(newContent);
-        };
-
-        triggerChange({
-            content: newContent,
-        });
+    if (value !== null) {
+      formContext.сhangeContent(name, newContent);
+      // setContent(newContent);
     }
 
-    const onClearContent = () => {
-        formContext.сhangeContent(null);
-        // setContent(null);
-        triggerChange({
-            content: null,
-        });
-    }
+    triggerChange({
+      content: newContent,
+    });
+  };
 
-    return (
-        <div style={{ display: 'flex' }}>
-            <Input value={/* value.content || content */ formContext.content} />
-            <Button icon={<EllipsisOutlined />} onClick={showDrawer} />
-            <Button icon={<CloseOutlined />} onClick={onClearContent} />
-            <Drawer
-                key="top"
-                placement="top"
-                closable={false}
-                onClose={onClose}
-                visible={visible}
-                height='80%'
-                destroyOnClose={true}
-                footer={
-                    <div style={{ textAlign: 'right' }} >
-                        <Button onClick={onClose}>Cancel</Button>
-                        <Button onClick={onClose} type="primary" >OK</Button>
-                    </div>
-                }
-            >
-                <Table
-                    dataSource={data}
-                    columns={columns}
-                    rowKey={record => record.id}
-                    onRow={record => {
-                        return {
-                            onClick: () => { onClickRow(record) },
-                            onDoubleClick: () => onClose()
-                        }
-                    }}
-                />
-            </Drawer>
-        </div>
-    );
-};
+  const onClearContent = () => {
+    formContext.сhangeContent(name, undefined);
+    // setContent(null);
+    triggerChange({
+      content: null,
+    });
+  };
+
+  return (
+    <div style={{ display: "flex" }}>
+      <Input
+        name={name}
+        value={/* value.content || content */ formContext.content[name]}
+      />
+      <Button icon={<EllipsisOutlined />} onClick={showDrawer} />
+      <Button icon={<CloseOutlined />} onClick={onClearContent} />
+      <Drawer
+        key="top"
+        placement="top"
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+        height="80%"
+        destroyOnClose={true}
+        footer={
+          <div style={{ textAlign: "right" }}>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button onClick={onClose} type="primary">
+              OK
+            </Button>
+          </div>
+        }
+      >
+        <Table
+          dataSource={data}
+          columns={columns}
+          rowKey={(record) => record.id}
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                onClickRow(record);
+              },
+              onDoubleClick: () => onClose(),
+            };
+          }}
+        />
+      </Drawer>
+    </div>
+  );
+}
--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,93 +1,93 @@
-import { useContext } from 'react';
-import { Form, Button } from 'antd';
-import { FormContext } from '../../Context/form.context';
-import { DrawerProvider } from '../../Context/drawer.context';
-import Drawer from '../Drawer';
+import { useContext } from "react";
+import { Form, Button } from "antd";
+import { FormContext } from "../../Context/form.context";
+import { DrawerProvider } from "../../Context/drawer.context";
+import Drawer from "../Drawer";
 
 const layout = {
-    labelCol: {
-        span: 8,
-    },
-    wrapperCol: {
-        span: 8,
-    },
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 8,
+  },
 };
 const tailLayout = {
-    wrapperCol: {
-        offset: 8,
-        span: 8,
-    },
+  wrapperCol: {
+    offset: 8,
+    span: 8,
+  },
 };
 
 export default function FormComponent() {
-    const formContext = useContext(FormContext);
+  const formContext = useContext(FormContext);
 
-    const onFinish = (values) => {
-        console.log('Success:', values);
-    };
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
 
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-    };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
 
-    const checkPrice = (_, value) => {
-        console.log('value: ', value);
-        if (value.content !== null && value.content !== '') {
-            return Promise.resolve();
-        }
+  const checkPrice = (_, value) => {
+    console.log("value: ", value);
+    if (value.content !== null && value.content !== "") {
+      return Promise.resolve();
+    }
 
-        return Promise.reject(new Error(_.message));
-    };
+    return Promise.reject(new Error(_.message));
+  };
 
-    return (
-        <Form
-            {...layout}
-            name="basic"
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            initialValues={{
-                department: { content: /* null */ formContext.content },
-                category: { content: formContext.content }
-            }}
-            style={{ marginTop: '10%' }}
-        >
-            <Form.Item
-                label="Department"
-                name="department"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please select department!',
-                        validator: checkPrice
-                    },
-                ]}
-            >
-                <DrawerProvider type={'department'}>
-                    <Drawer /* type={'department'} */ />
-                </DrawerProvider>
-            </Form.Item>
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      initialValues={{
+        department: { content: /* null */ formContext.content.department },
+        category: { content: formContext.content.category },
+      }}
+      style={{ marginTop: "10%" }}
+    >
+      <Form.Item
+        label="Department"
+        name="department"
+        rules={[
+          {
+            required: true,
+            message: "Please select department!",
+            validator: checkPrice,
+          },
+        ]}
+      >
+        <DrawerProvider type="department">
+          <Drawer /* type={'department'} */ name="department" />
+        </DrawerProvider>
+      </Form.Item>
 
-            <Form.Item
-                label="Category"
-                name="category"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please select category!',
-                        validator: checkPrice
-                    },
-                ]}
-            >
-                <DrawerProvider type={'category'}>
-                    <Drawer /* type={'category'} */ />
-                </DrawerProvider>
-            </Form.Item>
+      <Form.Item
+        label="Category"
+        name="category"
+        rules={[
+          {
+            required: true,
+            message: "Please select category!",
+            validator: checkPrice,
+          },
+        ]}
+      >
+        <DrawerProvider type="category">
+          <Drawer /* type={'category'} */ name="categry" />
+        </DrawerProvider>
+      </Form.Item>
 
-            <Form.Item {...tailLayout}>
-                <Button type="primary" htmlType="submit">
-                    Submit
-                </Button>
-            </Form.Item>
-        </Form>
-    );
-};
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
